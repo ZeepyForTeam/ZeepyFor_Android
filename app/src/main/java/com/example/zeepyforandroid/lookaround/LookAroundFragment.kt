60b191861bdd0287a3288b23ab5c48a6b0e3c689
unknown <jrsuh@fasoo.com>
2021-04-13 18:01:20 +0900

    map feature base

--- app/src/main/java/com/example/zeepyforandroid/lookaround/LookAroundFragment.kt
+++ app/src/main/java/com/example/zeepyforandroid/lookaround/LookAroundFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.zeepyforandroid.R
 
 class LookAroundFragment : Fragment() {
+
+    lateinit var button : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +23,9 @@ class LookAroundFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/zeepyforandroid/map/MapFragment.kt
@@ -0,0 +1,63 @@
+package com.example.zeepyforandroid.map
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.zeepyforandroid.R
+import com.naver.maps.map.MapView
+
+class MapFragment : Fragment() {
+
+    private lateinit var mapView: MapView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = view.findViewById(R.id.mv_NMap)
+        mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+}
