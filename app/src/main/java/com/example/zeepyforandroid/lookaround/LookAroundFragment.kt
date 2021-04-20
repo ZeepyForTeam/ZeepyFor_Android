a1783c183e0b0c71004eaf245f8f357d8107be4a
unknown <jrsuh@fasoo.com>
2021-04-20 16:08:56 +0900

    set custom markers

--- app/src/main/java/com/example/zeepyforandroid/lookaround/LookAroundFragment.kt
+++ app/src/main/java/com/example/zeepyforandroid/lookaround/LookAroundFragment.kt
@@ -1,31 +1,29 @@
 package com.example.zeepyforandroid.lookaround
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.navigation.Navigation
 import com.example.zeepyforandroid.R
+import com.example.zeepyforandroid.base.BaseFragment
+import com.example.zeepyforandroid.databinding.FragmentLookAroundBinding
 
-class LookAroundFragment : Fragment() {
 
-    lateinit var button : Button
+class LookAroundFragment : BaseFragment<FragmentLookAroundBinding>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_look_around, container, false)
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentLookAroundBinding {
+        return FragmentLookAroundBinding.inflate(inflater, container, false)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        button.setOnClickListener {
-
+        binding.btnMap.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_mainFrameFragment_to_mapFragment)
         }
     }
 }
--- app/src/main/java/com/example/zeepyforandroid/map/MapFragment.kt
+++ app/src/main/java/com/example/zeepyforandroid/map/MapFragment.kt
@@ -5,12 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
 import com.example.zeepyforandroid.R
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapView: MapView
+    private lateinit var naverMap : NaverMap
+
+    // 여러 개의 마커 선언 및 초기화
+    private var marker0 = Marker()
+    private var marker1 = Marker()
+    private var marker2 = Marker()
+    private var marker3 = Marker()
+    private var marker4 = Marker()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +38,17 @@ class MapFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         mapView = view.findViewById(R.id.mv_NMap)
         mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this) // Calls onMapReady
+
+
+    }
+
+    override fun onMapReady(@NonNull naverMap: NaverMap) {
+        this.naverMap = naverMap
+
+        setMarker(marker0, 37.5632424, 126.9834535, R.drawable.emoji_5_map)
+        setMarker(marker1, 37.5632500, 126.9836324, R.drawable.emoji_1_map)
+
     }
 
     override fun onStart() {
@@ -60,4 +85,13 @@ class MapFragment : Fragment() {
         super.onLowMemory()
         mapView.onLowMemory()
     }
+
+    fun setMarker(marker: Marker, lat: Double, lng: Double, resourceID: Int) {
+        // marker.isIconPerspectiveEnabled = true
+        marker.icon = OverlayImage.fromResource(resourceID)
+        // marker.alpha = 0.8f
+        marker.position = LatLng(lat, lng)
+        marker.map = naverMap
+    }
+
 }
