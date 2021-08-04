dd7caa0ec231c586349bb8deba08c2690724aa1a
ssrsph@naver.com <ssrsph@naver.com>
2021-08-04 13:01:26 +0900

    feat: 홈뷰 레이아웃

--- app/src/main/java/com/example/zeepyforandroid/home/HomeFragment.kt
+++ app/src/main/java/com/example/zeepyforandroid/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.zeepyforandroid.R
 import com.example.zeepyforandroid.base.BaseFragment
 import com.example.zeepyforandroid.databinding.FragmentHomeBinding
@@ -21,7 +22,28 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonNext.setOnClickListener {
+        setToolbar()
+        writeReview()
+
+    }
+
+    private fun setToolbar() {
+        binding.toolbar.apply {
+            setTitle("을지로 3가")
+            setCommunityLocation()
+            setRightButton(R.drawable.ic_btn_write) {
+                findNavController().navigate(R.id.action_communityMainFragment_to_communitySelectCategoryFragment)
+            }
+            setRightButtonMargin(32)
+
+            setOnClickListener {
+                requireParentFragment().requireParentFragment().findNavController().navigate(R.id.action_mainFrameFragment_to_communitySearchAddressFragment)
+            }
+        }
+    }
+
+    private fun writeReview() {
+        binding.buttonWriteReview.setOnClickListener {
             Navigation.findNavController(binding.root).navigate(R.id.action_mainFrameFragment_to_reviewFrameFragment)
         }
     }
