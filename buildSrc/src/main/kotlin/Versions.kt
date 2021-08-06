8f60769f89b42dfab01f1057ac4b3e48a04a7d22
jeongrok <shfo369@naver.com>
2021-08-06 23:29:50 +0900

    fixing dep version issues

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -30,4 +30,8 @@ object Versions {
     const val KAKAO_SDK_USER_VERSION = "2.5.2"
 
     const val KAKAO_USERMGMT_VERSION = "1.27.0"
+
+    const val MATERIAL_SPINNER_VERSION = "1.4.2"
+
+    const val ANDROID_DESIGN_VERSION = "28.0.0"
 }
