ece7746119c5e1ae1aa19936df439294b766215d
jeongrok <shfo369@naver.com>
2021-08-26 04:26:32 +0900

    fix: myprofile partial text change

--- app/src/main/java/com/zeepy/zeepyforandroid/myprofile/MyProfileFragment.kt
+++ app/src/main/java/com/zeepy/zeepyforandroid/myprofile/MyProfileFragment.kt
@@ -1,14 +1,22 @@
 package com.zeepy.zeepyforandroid.myprofile
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.SpannedString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.text.color
 import androidx.navigation.fragment.findNavController
 import com.zeepy.zeepyforandroid.R
 import com.zeepy.zeepyforandroid.base.BaseFragment
 import com.zeepy.zeepyforandroid.databinding.FragmentMyProfileBinding
 import com.zeepy.zeepyforandroid.myprofile.adapter.MyProfileOptionsAdapter
+import com.zeepy.zeepyforandroid.util.CustomTypefaceSpan
 
 
 class MyProfileFragment : BaseFragment<FragmentMyProfileBinding>() {
@@ -25,6 +33,7 @@ class MyProfileFragment : BaseFragment<FragmentMyProfileBinding>() {
 
         setButtonsOnClickListener()
         setOptionsRecyclerView()
+        changePartialText()
     }
 
     private fun setButtonsOnClickListener() {
@@ -46,5 +55,29 @@ class MyProfileFragment : BaseFragment<FragmentMyProfileBinding>() {
             setHasFixedSize(true)
             adapter = MyProfileOptionsAdapter(options)
         }
+    }
+
+    /**
+     * Changes part of the text's font type and color using annotations in strings.xml
+     * @see android.text.Annotation
+     */
+    private fun changePartialText() {
+        val text = getText(R.string.myprofile_loggedout) as SpannedString
+        val spannable = SpannableStringBuilder(text)
+        val annotations = text.getSpans(0, text.length, android.text.Annotation::class.java)
+
+        for (annotation in annotations) {
+            if (annotation.key == "font") {
+                val fontName = annotation.value
+                val typeface = ResourcesCompat.getFont(requireContext(), context?.resources!!.getIdentifier(fontName, "font", context?.packageName))
+                spannable.setSpan(CustomTypefaceSpan(typeface!!), spannable.getSpanStart(annotation), spannable.getSpanEnd(annotation), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
+            if (annotation.key == "color") {
+                val colorName = annotation.value
+                val colorId = context?.resources?.getIdentifier(colorName, "color", context?.packageName)
+                spannable.setSpan(ForegroundColorSpan(ContextCompat.getColor(requireContext(), colorId!!)), spannable.getSpanStart(annotation), spannable.getSpanEnd(annotation), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+        }
+        binding.tvMainMsg.text = spannable
     }
 }
