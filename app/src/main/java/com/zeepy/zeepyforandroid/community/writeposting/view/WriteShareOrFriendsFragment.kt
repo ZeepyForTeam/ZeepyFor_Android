9dd911fda346927d3b6dc351dddf3ed4e98c33f7
sonpyeonghwa <ssrsph@naver.com>
2021-08-30 02:49:00 +0900

    feat: 동네친구, 무료 나눔 플로우

--- app/src/main/java/com/zeepy/zeepyforandroid/community/writeposting/view/WriteShareOrFriendsFragment.kt
+++ app/src/main/java/com/zeepy/zeepyforandroid/community/writeposting/view/WriteShareOrFriendsFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.zeepy.zeepyforandroid.base.BaseFragment
 import com.zeepy.zeepyforandroid.community.writeposting.viewmodel.WriteShareOrFriendsViewModel
 import com.zeepy.zeepyforandroid.databinding.FragmentWriteShareOrFriendsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WriteShareOrFriendsFragment: BaseFragment<FragmentWriteShareOrFriendsBinding>() {
     private val viewModel: WriteShareOrFriendsViewModel by viewModels()
+    private val args: WriteShareOrFriendsFragmentArgs by navArgs()
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -20,14 +26,41 @@ class WriteShareOrFriendsFragment: BaseFragment<FragmentWriteShareOrFriendsBindi
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
         initView()
+        viewModel.changePostingType(args.postingType)
+        usableNextButton()
     }
 
     private fun initView() {
         binding.toolbarWriteShareOrFriends.apply {
             setTitle("글 작성하기")
             setBackButton{
+                findNavController().popBackStack()
+            }
+        }
+        binding.buttonNext.apply {
+            setText("다음으로")
+            setUnUsableButton()
+            onClick{
+                val action = WriteShareOrFriendsFragmentDirections
+                    .actionWriteShareOrFriendsFragmentToCommunityLoadPictureFragment(viewModel.requestWritePosting.value)
+                findNavController().navigate(action)
+            }
+        }
+    }
 
+    private fun usableNextButton() {
+        val inputList = listOf(viewModel.title, viewModel.content)
+        inputList.forEach {
+            it.observe(viewLifecycleOwner) {
+                if (viewModel.checkInputEveryField()) {
+                    binding.buttonNext.setCommunityUsableButton()
+                } else {
+                    binding.buttonNext.setUnUsableButton()
+                }
             }
         }
     }
--- app/src/main/java/com/zeepy/zeepyforandroid/community/writeposting/viewmodel/WriteShareOrFriendsViewModel.kt
+++ app/src/main/java/com/zeepy/zeepyforandroid/community/writeposting/viewmodel/WriteShareOrFriendsViewModel.kt
@@ -1,6 +1,86 @@
 package com.zeepy.zeepyforandroid.community.writeposting.viewmodel
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.zeepy.zeepyforandroid.base.BaseViewModel
+import com.zeepy.zeepyforandroid.community.controller.WritePostingController
+import com.zeepy.zeepyforandroid.community.data.remote.requestDTO.RequestWritePosting
+import com.zeepy.zeepyforandroid.enum.PostingType
+import com.zeepy.zeepyforandroid.localdata.ZeepyLocalRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
-class WriteShareOrFriendsViewModel: ViewModel() {
+@HiltViewModel
+class WriteShareOrFriendsViewModel@Inject constructor(
+    private val writePostingController: WritePostingController,
+    private val zeepyLocalRepository: ZeepyLocalRepository
+) : BaseViewModel() {
+    val title = MutableLiveData<String>()
+    val content = MutableLiveData<String>()
+
+    private val _postingType = MutableLiveData<String>()
+    val postingType: LiveData<String>
+        get() = _postingType
+
+    private val _selectedAddress = MutableLiveData<String>()
+    val selectedAddress: LiveData<String>
+        get() = _selectedAddress
+
+    private val _isEveryInfoEntered = MutableLiveData<Boolean>()
+    val isEveryInfoEntered: LiveData<Boolean>
+        get() = _isEveryInfoEntered
+
+    private val _uploadImages = MutableLiveData<List<String>>(listOf())
+    val uploadImages: LiveData<List<String>>
+        get() = _uploadImages
+
+    private val _requestWritePosting = MutableLiveData<RequestWritePosting>()
+    val requestWritePosting: LiveData<RequestWritePosting>
+        get() = _requestWritePosting
+
+    init {
+        fetchAddressList()
+    }
+
+    fun changePostingType(type: String) {
+        _postingType.value = type
+    }
+
+    fun checkInputEveryField(): Boolean {
+        val fieldList = listOf(title.value, content.value)
+        return fieldList.none { it.isNullOrEmpty() }
+    }
+
+    fun fetchAddressList() {
+        addDisposable(
+            zeepyLocalRepository.fetchAddressList()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    val selectedAddress = it.filter { it.isAddressCheck }.first().cityDistinct
+                    _selectedAddress.postValue(selectedAddress)
+                }, {
+                    it.printStackTrace()
+                })
+        )
+    }
+
+    fun sendRequestData() {
+        val requestData = RequestWritePosting(
+            selectedAddress.value!!,
+            PostingType.JOINTPURCHASE.name,
+            content.value,
+            uploadImages.value!!,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            title.value
+        )
+        _requestWritePosting.value = requestData
+    }
 }
