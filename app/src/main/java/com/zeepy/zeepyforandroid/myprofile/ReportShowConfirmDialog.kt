724f0f4ba28919e1b70a8ae28e7b857d9df284fb
roksui <shfo369@naver.com>
2021-09-24 17:58:15 +0900

    fix: change pwd

--- app/src/main/java/com/zeepy/zeepyforandroid/myprofile/ReportShowConfirmDialog.kt
+++ app/src/main/java/com/zeepy/zeepyforandroid/myprofile/ReportShowConfirmDialog.kt
@@ -25,29 +25,7 @@ fun reportShowConfirmDialog(fragment: Fragment) {
             override fun clickRightButton(dialog: ZeepyDialog) {
                 // TODO: 확인 누르면 신고 완료되었다는 텍스트 띄워주면 어떨지?
                 dialog.dismiss()
-//                if (fragment.findNavController().currentDestination?.id == R.id.reportOtherFragment)
-                    fragment.findNavController().popBackStack()
-            }
-        })
-        .build()
-        .show(fragment.childFragmentManager, fragment.tag)
-}
-
-fun changePasswordShowConfirmDialog(fragment: Fragment) {
-    val confirmDialog =
-        ZeepyDialogBuilder(fragment.resources.getString(R.string.change_password_confirm), "myProfile")
-
-    confirmDialog.setLeftButton(R.drawable.box_grayf9_8dp, "취소")
-        .setRightButton(R.drawable.box_yellowee_8dp, "비밀번호 변경")
-        .setButtonHorizontalWeight(0.287f, 0.712f)
-        .setDialogClickListener(object : DialogClickListener {
-            override fun clickLeftButton(dialog: ZeepyDialog) {
-                dialog.dismiss()
-            }
-
-            override fun clickRightButton(dialog: ZeepyDialog) {
-                // TODO: 비밀번호 변경 완료되었다는 텍스트 띄워주면 어떨지?
-                dialog.dismiss()
+                fragment.findNavController().popBackStack()
             }
         })
         .build()
